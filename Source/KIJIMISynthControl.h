3031c3f5ef84dc77c1f3d871b0e7208f56bf2dae
ffont <frederic.font@gmail.com>
2020-08-05 11:15:55 +0200

    Remove code for custom glide mode comming from DDRM

--- Source/KIJIMISynthControl.h
+++ Source/KIJIMISynthControl.h
@@ -91,25 +91,6 @@ public:
             }
         }
         
-        if (ID == "KIJIMI_GLIDE_MODE_GLIDE"){
-            // Custom behaviour for KIJIMI_GLIDE_MODE_GLIDE
-            // Note that some parameter info is hardcoded here and should be manually changed if needed
-            // b72=255 & b80=0 -> portamento, b72=0 & b80=255 -> glissando, b72=0 & b80 = 0 -> none
-            
-            // TODO: check this for KIJIMI, range might be 0-127
-            bool portamentoOn = bytes[72] > 127;  // Note range is 0-255 here
-            bool glissandoOn = bytes[80] > 127; // Note range is 0-255 here
-            
-            if (portamentoOn && !glissandoOn) {
-                return 0.0; // Portamento on
-            } else if (!portamentoOn && glissandoOn) {
-                return 1.0; // Glissando on
-            } else {
-                // If both off or both on, we consider none is active
-                return 0.5; // Both off
-            }
-        }
-        
         return -1.0;
     }
     
@@ -120,30 +101,6 @@ public:
         int byteValue = jlimit(0, 127, (int) round(normValue * 127));
         if ((byteNumber > -1) && (byteNumber < KIJIMI_PRESET_NUM_BYTES)){
             bytes[byteNumber] = byteValue;
-        }
-        
-        if (ID == "KIJIMI_GLIDE_MODE_GLIDE"){
-            // Custom behaviour for KIJIMI_GLIDE_MODE_GLIDE
-            // Note that some parameter info is hardcoded here and should be manually changed if needed
-            // b72=255 & b80=0 -> portamento, b72=0 & b80=255 -> glissando, b72=0 & b80 = 0 -> none
-            
-            // TODO: check this for KIJIMI, range might be different
-            
-            int midiValue = norm2midi((double)normValue);
-            
-            if (midiValue < 32) { // Follow KIJIMI MIDI spec
-                // Set to Portamento
-                bytes[72] = 255;
-                bytes[80] = 0;
-            } else if (midiValue >= 32 && midiValue < 85) {
-                // Set to None
-                bytes[72] = 255;
-                bytes[80] = 255;
-            } else if (midiValue >= 85) {
-                // Set to glissando
-                bytes[72] = 0;
-                bytes[80] = 255;
-            }
         }
     }
     
